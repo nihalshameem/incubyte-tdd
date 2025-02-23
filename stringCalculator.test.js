1e7f249feaffc19ec0f6a0982342f5f8fe67cfe7
nihalshameem <nihalshameem6@gmail.com>
2025-02-23 14:00:16 +0530

    add validation to throw error for negative numbers in add function and update tests

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -23,3 +23,7 @@ test("should handle newlines as delimiters", () => {
 test("should support custom delimiters", () => {
   expect(add("//;\n1;2")).toBe(3);
 });
+
+test("should throw an error for negative numbers", () => {
+  expect(() => add("1,-2,-3")).toThrow("negative numbers not allowed: -2,-3");
+});
