b165ac63da3370492618aa0dafbb6734d35a0604
nihalshameem <nihalshameem6@gmail.com>
2025-02-23 13:14:38 +0530

    add support for newlines as delimiters in add function and update tests

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,7 @@
 const add = (numbers) => {
     if (numbers === "") return 0;
-    const numArray = numbers.split(",").map((num) => parseInt(num, 10));
+    const normalized = numbers.replace(/\n/g, ",");
+    const numArray = normalized.split(",").map((num) => parseInt(num, 10));
     return numArray.reduce((sum, num) => sum + num, 0);
 };
 
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -15,3 +15,7 @@ test("should return the sum of two comma-separated numbers", () => {
 test("should return the sum of multiple numbers", () => {
   expect(add("1,2,3,4")).toBe(10);
 });
+
+test("should handle newlines as delimiters", () => {
+  expect(add("1\n2,3")).toBe(6);
+});
